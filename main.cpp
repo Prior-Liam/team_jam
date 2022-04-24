e76bca95aeb08cdb8f5d95a9ca0bc9b0bca09461
curtqikfox <curtis@qikfox.com>
2022-04-24 16:31:17 -0700

    Ask user for wave file, check if file can be opened

--- main.cpp
+++ main.cpp
@@ -2,61 +2,79 @@
 #include "Reverser.h"
 #include "Normalizer.h"
 
+#define PRINTX(x) std::cout << x << std::endl
+#define PRINT std::cout
+#define ENDL std::endl
+
+std::string GetUserWaveFile()
+{
+    std::string input;
+    const char *filePath;
+
+    std::cout << "Input wave file name: " << ENDL;
+    std::cin >> input;
+    return input;
+}
+
 int main()
 {
-    const char* filePath;
-    GetUserWaveFile(filePath);
+    std::string filePath;
+    bool does_wav_file_exist = false;
+
+    while (!does_wav_file_exist)
+    {
+        filePath = GetUserWaveFile();
+        PRINTX("File received from user: " << filePath);
+
+        std::ifstream file(filePath, std::ios::binary | std::ios::in);
+        short *buffer = nullptr;
+        if (file.is_open()) {
+            PRINTX("File Successfully opened");
+            does_wav_file_exist = true;
+        }
+    }
 
     return 0;
 }
 
-bool GetUserWaveFile(const char* filePath) {
-    string input;
+// void WaveFileManager::read(const std::string &fileName)
+// {
+// std::ifstream file(fileName, std::ios::binary | std::ios::in);
+// short* buffer = nullptr;
+// if (file.is_open())
+// {
+//     file.read((char*) &waveHeader, sizeof(wav_header));
+//     buffer = new short [waveHeader.data_bytes];
+//     file.read((char*) buffer, waveHeader.data_bytes);
 
-    cout << "Input wave file name: ";
-    cin >> input;
-    cin.get();
-    filePath = input.c_str();
-}
-
-void WaveFileManager::read(const std::string &fileName)
-{
-    // std::ifstream file(fileName, std::ios::binary | std::ios::in);
-    // short* buffer = nullptr;
-    // if (file.is_open())
-    // {
-    //     file.read((char*) &waveHeader, sizeof(wav_header));
-    //     buffer = new short [waveHeader.data_bytes];
-    //     file.read((char*) buffer, waveHeader.data_bytes);
-    
-    //     for (int i = 0; i < waveHeader.data_bytes / waveHeader.sample_alignment; i++)
-    //     {
-    //         soundData.push_back((float) buffer[i] / MAX_16BIT);
-    //     }
-    // file.close();
-    // }
-    // else
-    // {
-    //     std::cout << "File not found" << std::endl;
-    // }
-    // delete[] buffer;
-}
+//     for (int i = 0; i < waveHeader.data_bytes / waveHeader.sample_alignment; i++)
+//     {
+//         soundData.push_back((float) buffer[i] / MAX_16BIT);
+//     }
+// file.close();
+// }
+// else
+// {
+//     std::cout << "File not found" << std::endl;
+// }
+// delete[] buffer;
+// }
 
 typedef struct wav_header
 {
-  char chunkID[4];
-  int chunkSize;
-  char format[4];
+    char chunkID[4];
+    int chunkSize;
+    char format[4];
 
-  char subchunk1ID[4];
-  int subchunk1Size;
-  short audioFormat;
-  short numChannels;
-  int sampleRate;
-  int byteRate;
-  short blockAlign;
-  short bitsPerSample;
+    char subchunk1ID[4];
+    int subchunk1Size;
+    short audioFormat;
+    short numChannels;
+    int sampleRate;
+    int byteRate;
+    short blockAlign;
+    short bitsPerSample;
 
-  char subchunk2ID[4];
-  int subchunk2Size;
+    char subchunk2ID[4];
+    int subchunk2Size;
 } WAV_HEADER;
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-main: main.cpp processors.o
-	g++ -std=c++11 -o wave processors.o main.cpp
+main: main.cpp #processors.o
+	g++ -std=c++11 -o wave main.cpp
 
 processors.o: Echo.cpp Echo.h Reverser.cpp Reverser.h Normalizer.cpp Normalizer.h
 	g++ -std=c++11 -c Echo.h Reverser.cpp Reverser.h Normalizer.cpp Normalizer.h
